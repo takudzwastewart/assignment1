4f1b6f9ffc2e7471fad46cc95ece2ef488a21a48
takudzwastewart <86067049+takudzwastewart@users.noreply.github.com>
2021-06-18 15:32:25 +0200

    Add files via upload

---
+++ table.js
@@ -0,0 +1,11 @@
+function myFuction()
+{
+    var x = document.getElementsByClassName("zbr");
+
+for (var i = 0; i <x.length;i++)
+{
+    x[i].style.backgroundColor = "black";
+    x[i].style.color = "white";
+}
+
+}
